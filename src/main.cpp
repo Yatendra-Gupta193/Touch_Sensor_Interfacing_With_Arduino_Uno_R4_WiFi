ed8546d12eb185a98c4fa9a739bac36c8c97cc68
Yatendra-Gupta193 <guptajikrish12@gmail.com>
2026-04-11 10:16:05 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+// Pin initialization 
+int touchPin = 2;   // Touch sensor OUT pin
+int ledPin = 13;    // LED pin
+
+void setup() {
+   // write your initialization code here
+   pinMode(touchPin, INPUT);
+   pinMode(ledPin, OUTPUT);
+   Serial.begin(9600);
+}
+
+void loop() {
+    // write your code here
+    
+    int touchState = digitalRead(touchPin);
+
+    if (touchState == HIGH) {
+          digitalWrite(ledPin, HIGH);  // LED ON
+          Serial.println("Touched!");
+    } else {
+         digitalWrite(ledPin, LOW);   // LED OFF
+         Serial.println("Not touched");
+    }
+
+  delay(100);
+
+}
